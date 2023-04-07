48ac522bb4e828b4b85118b3a4b7d2301073fd75
Daniela Irene Garzón <digarzonp@unal.edu.co>
2023-04-06 22:13:23 -0500

    version 2.0

---
+++ programa.js
@@ -0,0 +1,22 @@
+//Recordar Javascript
+
+// comentarios de una linea, clarificar una expresion 
+
+/*
+COmentario de 
+varias lineas
+DOcumentar el código
+*/
+
+//No es necesario declara una variable antes de utilizar (es recomendable)
+//variable:lugar en la memoria del computador al cual le coloco nombre, y lo uso para guardar un valor 
+
+//declarar una variable
+//variable  de ambito de función
+var a;
+
+//variables de ambito de bloque 
+let b;
+
+//operaciones: aritmeticas, comparación, lógica binaria  (AND, OR)
+//Estructuras de datos: arreglos... guardar mas de una variable 
